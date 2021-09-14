8eaa77c1aafa60fe8d877a67e18f29a95142d3ae
Shiro94979 <89897054+Shiro94979@users.noreply.github.com>
2021-09-15 00:39:55 +0700

    Add files via upload

---
+++ App/src/pages/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+
+export class Main extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Hi  </Text>
+            </View>
+        )
+    }
+}
+
+export default Main
