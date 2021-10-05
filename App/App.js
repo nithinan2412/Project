11096565964747ba5bbffd3f05d280ee7bc59296
Shiro94979 <89897054+Shiro94979@users.noreply.github.com>
2021-10-05 22:05:00 +0700

    Add files via upload

--- App/App.js
+++ App/App.js
@@ -1,58 +1,49 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 
-import Login from './src/pages/Login';
+import React, { Component } from 'react'
+import LoginScreen from './src/pages/LoginScreen'
+import RegScreen from './src/pages/RegScreen'
+import HomeScreen from './src/pages/HomeScreen'
+import calendarScreen from './src/pages/calendarScreen'
+import ProfileScreen from './src/pages/ProfileScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
 
-  
-  render() {
-    return (
-      <View style ={styles.container}>
-        <Login/>
-      </View>
-    );
-  }
+
+const Stack = createStackNavigator()
+
+function App() {
+
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RegScreen"
+          component={RegScreen}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="calendarScreen"
+          component={calendarScreen}
+        />
+        <Stack.Screen
+          name="ProfileScreen"
+          component={ProfileScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    backgroundColor: '#42a5f5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#0077c2",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-
-  inputView:{
-    width:"80%",
-    backgroundColor:"#ffff",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  textcolor:{
-    color:"#f000"
-  }
-});
-export default App;
+export default App
